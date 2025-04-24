129b64473baaf3abf22037bb6b2d658a744f29b8
VyshnaviDeloitte <vvelishala@deloitte.com>
2025-04-24 14:14:59 +0530

    Implement table to display quizzes created by admin

--- src/app/components/admin-quiz-table/admin-quiz-table.component.ts
+++ src/app/components/admin-quiz-table/admin-quiz-table.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-quiz-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-quiz-table.component.html',
   styleUrl: './admin-quiz-table.component.css'
 })
 export class AdminQuizTableComponent {
+
+  quizzes: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('http://localhost:3000/quizzes').subscribe(data => {
+      this.quizzes = data;
+    });
+  }
+
+  viewResults(quiz: any) {
+    console.log('Viewing results for quiz:', quiz.title);
+    alert(`Results would open for quiz: ${quiz.title}`);
+    // You can later open a modal or route to a detail page
+  }
 
 }
